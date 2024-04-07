dceae1f7e7c37fd3a6c5256663b8ca58ee771b52
filip.vanelslande@gmail.com <filip.vanelslande@gmail.com>
2024-04-07 22:07:20 +0200

    update password worksheet whit data array

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('password_manager')
 
+
 def get_passwords():
     """
     Get user data password information
@@ -26,12 +27,22 @@ def get_passwords():
     data_string = input("Enter your data here: ")
     print(f"The password data is : {data_string}")
 
+    worksheet_to_update = SHEET.worksheet("passwords")
+    worksheet_to_update.append_row(data_string.split(',')) 
+
+    print(f"Password worksheet updated successfully\n")
+  
+
+
+    
+
 def main():
     """
     Run all program functions    
     """
     get_passwords()
 
+# welcome text
 print("Welcome to Password Manager")
-main()
+main()  
     
