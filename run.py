93cdea22aa3171eb885f26ee2c716cc6f7f4ca66
filip.vanelslande@gmail.com <filip.vanelslande@gmail.com>
2024-04-11 15:22:25 +0200

    debug menu option 4 by check if number

--- run.py
+++ run.py
@@ -474,8 +474,9 @@ def menu_option_4():
         input(f"Press Enter to continue ... \n")
         os.system("clear")
     else:
-        # print(check_if_number(index_number))
-        if check_if_number(index_number):
+        print(check_if_number(int(index_number)))
+        input("Press Enter to continue...")
+        if check_if_number(int(index_number)):
             copy_password_entry(int(index_number))
         else:
             emptyblock()
